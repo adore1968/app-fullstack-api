9d222e78d3796fd8c8117c46be3fbf05fc86c6c9
José <jg345762@gmail.com>
2023-07-26 16:46:04 -0300

    src/database/connection.js updated

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,7 +4,11 @@ const uri = process.env.MONGO_URI;
 
 const connect = async () => {
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
     console.log("Connected to the database");
   } catch (error) {
     console.log(error);
